587a82e0e7ce6b5ae9daa13b29212dee10ab09ed
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2022-10-10 00:27:22 -0400

    dchanges

--- design.js
+++ design.js
@@ -1,5 +1,35 @@
-const voteforachange = document.getElementById('.firstvote1');
-;
-for (let i = 0; i < 10; i++) {
-    console.log('voteforachange');
+
+function changeColor(x) {
+    if (x < 5) {
+        document.querySelector('#firstvote1').style.color = "green";
+    }
+
+    else if (x == 6) {
+        document.querySelector('#firstvote1').style.color = "red";
+    }
 }
+
+
+const span = document.querySelector('#firstvote1');
+let htmlString = "";
+
+for (let i = 0; i < 10; i++) {
+
+
+    if (i % 2 == 0) {
+        changeColor(i);
+        htmlString += '<span>Vote For a change </span>';
+
+    }
+
+    else if (i % 2 !== 0) {
+
+        htmlString += '<span>You doing the right thing </span>';
+    }
+
+}
+span.innerHTML = htmlString;
+
+
+
+
