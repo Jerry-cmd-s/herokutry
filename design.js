95510d28512aa252d007086d29fa76e32c1643be
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2022-10-26 21:59:23 -0400

    responsive nab=vbar

--- design.js
+++ design.js
@@ -1,42 +1,11 @@
+//creating an anonymous functon dont know why 
+const navslide = () => {
 
-function changeColor(x) {
+    const burger = document.querySelector('.burger');
+    const votenav = document.querySelector('.votenavcontents');
 
-
-
-
-
-    if (x % 2 == 0) {
-        3
-        document.querySelector('#firstvote1').style.color = "green";
-    }
-
-    else if (x % 2 !== 0) {
-        document.querySelector('#firstvote1').style.color = "red";
-    }
+    burger.addEventListener('click', () => {
+        votenav.classList.toggle('nav-activ');
+    });
 }
-
-
-const span = document.querySelector('#firstvote1');
-let htmlString = "";
-
-for (let i = 0; i < 10; i++) {
-
-    changeColor(i)
-    //if (i % 2 == 0) {
-
-
-    htmlString += '<span>Vote For a change </span>';
-
-    //}
-
-    // else if (i % 2 !== 0) {
-
-    //     htmlString += '<span>You doing the right thing </span>';
-    // }
-
-}
-span.innerHTML = htmlString;
-
-
-
-
+navslide();
