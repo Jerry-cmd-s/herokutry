b8125fabcd1c6c7581b359a8433dea58fc97cb94
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2022-12-12 21:19:04 -0500

    aws servers config

--- datasvoter.php
+++ datasvoter.php
@@ -1,52 +1,35 @@
 <?php
 $host ="votingdatabase.coispubgurww.us-east-1.rds.amazonaws.com";
 $username = "admin";
-$password ="Pauljina20022003#";
-$dbname = "votingdatabase";
+$password ="Dsafdfwr3r3e3e3edds2#";
+$dbname = "votingdatas";
 
 
 
 //assigning users datas into variables
-
-$lname = $_POST['lname'];
-$fname= $_POST['fname'];
-$sex= $_POST['sex'];
-$ethnicity= $_POST['ethnicity'];
-$age= $_POST['age'];
-$candidates= $_POST['candidates'];
-$terms= filter_input(INPUT_POST,"terms,FILTER_VALIDATE_BOOL");
+//
 
 
 //Now i am checking if it has the terms box had been checked
-if (!$terms)
-{
 
-    die("Terms must be accepted");
-}
 
 //Now, I am going to create a connection 
 $conn = new mysqli($host,$username,$password,$dbname);
 
+
+
 //Now,I have to check my connection
-if ($conn->connect_error)
+if (!$conn)
 {
-    die("Connection failed:" .$conn->connect_error);
+    die("Connection failed:" .mysql.error());
 }
 
-    $sql = "INSERT INTO voters(lastname,firstname,sex,ethnicity,age,candidates)
-    Values('$lname', '$fname', '$sex', '$ethnicity', $age, '$candidates')";
-
-    $insert=mysqli_query($conn,$sql);
-    
-        if(!$insert) 
-    {
-        echo "theres a problem niggah";
-
-    }
     else
-{
-    echo"You voted for a change Congratulation!!";
-}
+    {
+        echo"You voted for a change Congratulation!!";
+    }
+
+   
 
 ?>
 
