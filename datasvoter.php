e61e5fd6bc10ac8d6ae0df7204bb391b57e91b8f
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2022-12-14 14:55:42 -0500

    d

--- datasvoter.php
+++ datasvoter.php
@@ -1,35 +1,6 @@
 <?php
-$servername ="votingdatabase.coispubgurww.us-east-1.rds.amazonaws.com";
-$username = "root";
-$password ="Dsafdfwr3r3e3e3edds2#";
-$dbname = "votingdatas";
-
-
-
-//assigning users datas into variables
-//
-
-
-//Now i am checking if it has the terms box had been checked
-
-
-//Now, I am going to create a connection 
-$conn = new mysqli($servername,$username,$password,$dbname);
-
-
-
-//Now,I have to check my connection
-if (!$conn)
-{
-    die("Connection failed:" .mysql.error());
-}
-
-    else
-    {
         echo"You voted for a change Congratulation!!";
-    }
 
-   
 
 ?>
 
