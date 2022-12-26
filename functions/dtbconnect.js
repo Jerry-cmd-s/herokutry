cd835eab716eb0368058c92fdf8f4f907cf94e89
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2022-12-26 18:16:50 -0500

    ff

---
+++ functions/dtbconnect.js
@@ -0,0 +1,31 @@
+const mysql = require('msql')
+
+
+
+exports.handler = async (event, context) => {
+    const connection = mysql.createconnection({
+        host="votingdatabase.coispubgurww.us-east-1.rds.amazonaws.com";
+        username = "myvotingdatabse",
+        password ="Dsafdfwr3r3e3e3edds2#",
+        dbname = "votingdatas"
+
+    });
+    connection.connect(function (err) {
+        if (err) throw err;
+        else
+            console.log('CONNECTED');
+    });
+
+
+
+
+
+
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello world" }),
+    };
+
+
+};
