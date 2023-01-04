f2a732dd7180861f61d6a5ef8ce74904d128247d
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2023-01-03 22:36:56 -0500

    gf

--- functions/dtbconnect.js
+++ functions/dtbconnect.js
@@ -7,7 +7,7 @@ const mysql = require('msql')
 exports.handler = async (event, context) => {
     const connection = mysql.createconnection({
         host: "myvotingdatabse.coispubgurww.us-east-1.rds.amazonaws.com",
-        username: "jerrycamidb",
+        username: "admin",
         password: "Dsafdfwr3r3e3e3edds2#",
         dbname: "myvotingdatabse"
 
