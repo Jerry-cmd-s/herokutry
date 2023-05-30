19a3bae257935df99b1b2dfb95e9af1ee2871de0
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2023-05-29 22:59:12 -0400

    r

--- function/dtbconnect.js
+++ function/dtbconnect.js
@@ -22,7 +22,7 @@ exports.handler = async (event) => {
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Error inserting data' }),
+            body: JSON.stringify(error),
         };
     } finally {
         connection.end();
