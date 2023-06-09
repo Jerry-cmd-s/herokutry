f4f6b5b450ffc3fcbda4a9949a829c2e6264a4d3
Jerry-cmd-s <jerry.cami.jeanbaptiste@gmail.com>
2023-06-09 02:22:20 -0400

    k

--- functions/dtbconnect.js
+++ functions/dtbconnect.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
         host: 'elvish-cow-4060.g8z.cockroachlabs.cloud',
         user: 'jerrycami',
         password: 'tOMLDsvMX2Kile2fq4SpRQ',
-        database: 'clientdtb',
+        database: 'clientdb',
         port: '26257'
     });
 
